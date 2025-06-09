ad7ea0a576bb5b060c5556002f5f39804b1f3580
pauchok-sk <pauchoooooook@gmail.com>
2025-06-09 22:35:31 +0300

    repair min

--- dist/js/app.js
+++ dist/js/app.js
@@ -27,21 +27,6 @@
             title.appendChild(span);
         }));
     }
-    function footerResize() {
-        function adjustFooterForSafari() {
-            const footer = document.querySelector(".footer");
-            if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-                const isSafari = /^((?!CriOS).)*Safari/.test(navigator.userAgent);
-                if (isSafari) {
-                    const vh = window.innerHeight * .01;
-                    document.documentElement.style.setProperty("--vh", `${vh}px`);
-                    footer.style.paddingBottom = "calc(20px + env(safe-area-inset-bottom))";
-                }
-            }
-        }
-        window.addEventListener("resize", adjustFooterForSafari);
-        window.addEventListener("DOMContentLoaded", adjustFooterForSafari);
-    }
     function initSingleSliders() {
         const blocks = document.querySelectorAll(".wp-block-gallery");
         if (blocks.length) blocks.forEach((block => {
@@ -321,5 +306,4 @@
     editSingle();
     initSingleSliders();
     modalImages();
-    footerResize();
 })();
